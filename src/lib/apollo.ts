c28edc11c7562fe38fa69ac331543b8a6edeac07
lacj2000 <luizjuniordeveloper@gmail.com>
2022-06-21 20:24:24 -0300

    update - apollo client

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const CONTENT_API = 'https://api-sa-east-1.graphcms.com/v2/cl4nmh42g0jsz01xm0zjt34mu/master';
+
 export const client = new ApolloClient({
-  uri:'https://api-sa-east-1.graphcms.com/v2/cl4nmh42g0jsz01xm0zjt34mu/master',
+  uri: CONTENT_API,
   cache: new InMemoryCache()
 })
