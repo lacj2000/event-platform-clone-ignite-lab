741e0ecdf06f070caefc2bd69f297cd607e3ac2c
lacj2000 <luizjuniordeveloper@gmail.com>
2022-06-22 01:27:21 -0300

    update - structuring event page

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,7 +1,15 @@
 import { Header } from "../Components/Header";
+import { Sidebar } from "../Components/Sidebar";
+import { Video } from "../Components/Video";
 
 export function Event() {
   return (
-    <Header />
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
   )
 } 
