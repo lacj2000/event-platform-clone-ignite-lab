b42ea9e84b2547fee2a223d65b35dc1c0a254498
lacj2000 <luizjuniordeveloper@gmail.com>
2022-06-25 03:07:44 -0300

    update - refatoring LessonComponent

--- src/Components/Lesson.tsx
+++ src/Components/Lesson.tsx
@@ -13,119 +13,62 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
-  const { slug: SelectedSlug } = useParams<{slug : string}>();
+  const { slug: selectedSlug } = useParams<{slug : string}>();
   
   const availableDataFormated = format(props.availableAt,"EEEE' • 'd' de 'LLLL' • 'kk'h'mm", {
     locale:ptBR,
   });
   const isLessonAvailable = isPast(props.availableAt);
   
+  const isSelectedSlug = selectedSlug === props.slug;
+
+
 
   return (
-    <>
-      {isLessonAvailable?(
-        <Link to={`/event/lesson/${props.slug}`} className="group" >
-          
-          <span className="text-gray-300">
-            {availableDataFormated[0].toUpperCase()+availableDataFormated.slice(1)}
-          </span>
-          
-          <div 
-           className={props?.slug===SelectedSlug ? 
-            "bg-green-300 rounded border border-green-300   p-4 gap-4 mt-2 "
-            :
-            "rounded border border-gray-600 p-4 gap-4 mt-2 group-hover:border-green-300 transition-colors"
-           }
+    <Link 
+     to={`${isLessonAvailable ? `/event/lesson/${props.slug}`:""}`} 
+     className={`group`} 
+    >
+      <div className={`${isSelectedSlug ?"relative  right-[7.5px] top-[81px] rotate-45 h-[15px] w-[15px] bg-green-300": "bg-transparent " } transition-colors`} ></div>
+      <span className="text-gray-300" >
+        {availableDataFormated}
+      </span>
+      <div 
+       className={`rounded border border-gray-300 mt-2 p-4 gap-4 m-0 w-full ${isLessonAvailable?"group-hover:border-green-300":"group-hover:border-gray-300"} ${
+        isSelectedSlug? "border-green-300 bg-green-300  " 
+        : ""} transition-colors`}
+      >
+        <header className='flex items-center justify-between '>
+          <span 
+          className={`text-sm flex font-medium gap-2 items-center ${isLessonAvailable? "text-blue-500":"text-orange-500"} ${isSelectedSlug&&"text-white"}`}
           >
-            <header className="flex items-center justify-between">  
-              {isLessonAvailable?(
-                <span 
-                 className={props?.slug===SelectedSlug ?
-                  "text-sm text-white font-medium flex items-center gap-2"
-                  :
-                  "text-sm text-blue-500 font-medium flex items-center gap-2"
-                 }
-                >
-                  <CheckCircle size={20} />
-                  Conteúdo Liberado
-                </span>
-              ):(
-                <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-                  <LockKey size={20} />
-                  Em breve
-                </span>
-              )}
-                
-              
-              {props.type === 'live'? (
-                <span 
-                 className={props?.slug===SelectedSlug ?
-                  "text-xs rounded py-[0.125rem] px-2  border border-white p-[1px] text-white"
-                  :
-                  "text-xs rounded py-[0.125rem] px-2  border border-green-300 p-[1px] text-white"
-                 }
-                >
-                  AO VIVO
-                </span>
-              ) : (
-                <span 
-                 className={props?.slug===SelectedSlug ?
-                  "text-xs rounded py-[0.125rem] px-2  border border-white p-[1px] text-white"
-                  :
-                  "text-xs rounded py-[0.125rem] px-2  border border-green-300 p-[1px] text-white"
-                 }
-                >
-                AULA PRÁTICA
-                </span>
-              )}
-            </header>
-            
-            <strong 
-             className={props?.slug===SelectedSlug ? 
-              "text-white mt-4 block":
-              "text-gray-200 mt-4 block"
-             }
-            >
-              {props.title}
-            </strong>
-          </div>
-        </Link>
-      ):(
-        // blocked
-        <Link 
-         to={`/event/lesson/${props.slug}`} 
-         className="group"  
-         onClick={(e)=>e.preventDefault()}
-        >
-          
-          <span className="text-gray-300">
-            {availableDataFormated[0].toUpperCase()+availableDataFormated.slice(1)}
+            {isLessonAvailable?(
+              <>
+                <CheckCircle />
+                {"Conteúdo Liberado"}
+              </>
+            ):(
+              <>
+                <LockKey size={24}/>
+                {"Em breve"}
+              </>
+            )}
           </span>
-          
-          <div className="rounded border border-gray-600 p-4 gap-4 mt-2 ">
-            <header className="flex items-center justify-between">  
-              <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-                <LockKey size={20} />
-                Em breve
-              </span>
-              
-              {props.type === 'live'? (
-                <span className="text-xs rounded py-[0.125rem] px-2  border border-green-300 p-[1px] text-white">
-                  AO VIVO
-                </span>
-              ) : (
-                  <span className="text-xs rounded py-[0.125rem] px-2  border border-green-300 p-[1px] text-white">
-                  AULA PRÁTICA
-                </span>
-              )}
-            </header>
-            
-            <strong className="text-gray-200 mt-4 block">
-              {props.title}
-            </strong>
-          </div>
-        </Link>
-      )}
-    </>
-  )
-} 
+
+          <span
+          className={`text-xs uppercase font-bold text-white py-[0.125rem] px-2 border rounded ${isSelectedSlug? "border-white": "border-green-300"}`}
+          >
+            {props.type==="class"?
+              "Aula prática"
+            :
+              "Ao vivo"
+            }
+          </span>
+        </header>
+        <strong className={`mt-4 text-gray-200 ${isSelectedSlug&&"text-white"} block`}>
+          {props.title}
+        </strong>
+      </div>
+    </Link>
+  );
+}
